b09726e9bbe45d5382c1cdf6be569d50efa0813b
RIfadN23 <76134523+RIfadN23@users.noreply.github.com>
2023-03-16 16:40:59 -0600

    Proyecto guardado

---
+++ templates/Login/SeccionForm.php
@@ -0,0 +1,43 @@
+
+<section>
+    <div class="page-header min-vh-75">
+      <div class="container">
+        <div class="row justify-content-center align-items-center vh-100">
+          <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+            <div class="card card-plain mt-8">
+              <div class="card-header pb-0 text-left bg-transparent">
+                <h3 class="font-weight-bolder text-info text-gradient">Bienvenido!</h3>
+                <p class="mb-0">Ingresa tu correo y contraseña para acceder</p>
+              </div>
+              <div class="card-body">
+                <form role="form">
+                  <label>Correo</label>
+                  <div class="mb-3">
+                    <input type="email" class="form-control" placeholder="correo@uaem.com" aria-label="Email" aria-describedby="email-addon">
+                  </div>
+                  <label>Contraseña</label>
+                  <div class="mb-3">
+                    <input type="password" class="form-control" placeholder="***********" aria-label="Password" aria-describedby="password-addon">
+                  </div>
+                  <div class="text-center">
+                    <button type="button" class="btn bg-gradient-info w-100 mt-4 mb-0">Iniciar sesión</button>
+                  </div>
+                </form>
+              </div>
+              <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                <p class="mb-4 text-sm mx-auto">
+                  Software creado por
+                  <a href="javascript:;" class="text-info text-gradient font-weight-bold">ICO y LIA CUX</a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+              <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('../assets/img/curved-images/fondoLog.jpg')"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
